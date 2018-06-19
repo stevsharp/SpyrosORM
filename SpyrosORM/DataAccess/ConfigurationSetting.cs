1150dcbfd51b4059ea095b2b345360d8612418e4
Spiros Ponaris <sponaris@realconsulting.gr>
2018-06-19 16:53:20 +0300

    Configuration

---
+++ SpyrosORM/DataAccess/ConfigurationSetting.cs
@@ -0,0 +1,39 @@
+﻿using System.Configuration;
+
+namespace SpyrosORM.DataAccess
+{
+    public class ConfigurationSetting
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public Configuration DllConfig { get;  }
+        /// <summary>
+        /// 
+        /// </summary>
+        public ConfigurationSetting()
+        {
+            DllConfig = ConfigurationManager.OpenExeConfiguration(this.GetType().Assembly.Location);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sectionName"></param>
+        /// <returns></returns>
+        public AppSettingsSection LoadConfigsSection(string sectionName)
+        {
+            return (AppSettingsSection)DllConfig.GetSection(sectionName);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sectionName"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public string GetConfigValue(string sectionName, string key)
+        {
+            return LoadConfigsSection(sectionName).Settings[key].Value;
+        }
+
+    }
+}
